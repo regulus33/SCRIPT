937be1efc76a515f0b7518d9024d67ba8a9a5951
zachary <zachary.rowden@showmax.com>
2024-01-02 18:50:49 +0100

    make attribute_anon_commits script work even though its not great

--- attribute_anon_commits.sh
+++ attribute_anon_commits.sh
@@ -1,38 +1,25 @@
+# WARNING: this does not preserve the commit dates... its not that useful
 #!/bin/bash
 
 # Backup your repository before running this script!
 
-# The email you want to change FROM (your other email)
-GIT_EMAIL="wrong_email_to_replace@example.com"
-
-# The name and email you want to change TO (your personal details)
-CORRECT_NAME="Zachary Rowden"
-CORRECT_EMAIL="zacharyrowden89@gmail.com"
+# new author of all commits.
+export GIT_AUTHOR_NAME="Zachary Rowden"
+export GIT_AUTHOR_EMAIL="zacharyrowden89@gmail.com"
 
 # Function to rewrite history
 rewrite_history() {
-    git filter-branch --env-filter '
-    OLD_EMAIL="$GIT_EMAIL"
-    CORRECT_NAME="$CORRECT_NAME"
-    CORRECT_EMAIL="$CORRECT_EMAIL"
-
-    if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-    then
-        export GIT_COMMITTER_NAME="$CORRECT_NAME"
-        export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-    fi
-    if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-    then
-        export GIT_AUTHOR_NAME="$CORRECT_NAME"
-        export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-    fi
-    ' --tag-name-filter cat -- --branches --tags
+    git rebase -r --root --exec "git commit --amend --no-edit --reset-author"
 }
 
 # Main execution
 echo "Starting to rewrite history..."
 rewrite_history
 echo "History rewrite complete."
+
+# Unset the temporary author details
+unset GIT_AUTHOR_NAME
+unset GIT_AUTHOR_EMAIL
 
 # Reminder for pushing changes
 echo "Don't forget to force push your changes to GitHub with: git push --force --tags origin 'refs/heads/*'"
--- stuff_to_put_in_ur_bash_profile.sh
+++ stuff_to_put_in_ur_bash_profile.sh
@@ -1,4 +1,4 @@
-copy_to_clipboard() {
+copy_to_clipboard_linux() {
     if [ -f "$1" ]; then
         xclip -selection clipboard < "$1"
         echo "Contents of $1 copied to clipboard."
@@ -15,6 +15,27 @@ copy_to_clipboard_mac() {
         echo "Error: File not found."
     fi
 }
+
+paste_from_clipboard_linux() {
+    if [ $# -eq 2 ]; then
+        filename="$1.$2"
+        xclip -selection clipboard -o > "$filename"
+        echo "Clipboard contents saved to $filename."
+    else
+        echo "Error: Please specify a filename and an extension."
+    fi
+}
+
+paste_from_clipboard_mac() {
+    if [ $# -eq 2 ]; then
+        filename="$1.$2"
+        pbpaste > "$filename"
+        echo "Clipboard contents saved to $filename."
+    else
+        echo "Error: Please specify a filename and an extension."
+    fi
+}
+
 
 
 
