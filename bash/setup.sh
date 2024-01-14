735a02e59a641f899ac89effb219199932f24484
regulus33 <zacharyrowden89@gmail.com>
2024-01-14 20:20:52 +0100

    s

---
+++ bash/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# Make all .sh files in the current directory executable
+chmod +x *.sh
+
+# Get the full path of the current directory
+DIR="$(pwd)"
+
+# Check if the directory is already in the PATH
+if ! grep -q "$DIR" ~/.zshrc; then
+    # Add the directory to the PATH
+    echo "export PATH=\"$DIR:\$PATH\"" >> ~/.zshrc
+    echo "Directory $DIR added to PATH in ~/.zshrc"
+
+    # Apply the changes
+    source ~/.zshrc
+else
+    echo "Directory $DIR is already in PATH."
+fi
